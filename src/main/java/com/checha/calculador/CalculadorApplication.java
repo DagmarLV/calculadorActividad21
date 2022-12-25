2356b07b8a60638054f540f035361452eb3d1a47
DagmarLV <dagmar.lezama.v@uni.pe>
2022-12-25 18:36:54 -0500

    Comentario prueba trigger

--- src/main/java/com/checha/calculador/CalculadorApplication.java
+++ src/main/java/com/checha/calculador/CalculadorApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculadorApplication {
+	//Este es un comentario para probar que la construcción es automática
 	public static void main(String[] args) {
 		SpringApplication.run(CalculadorApplication.class, args);
 	}
