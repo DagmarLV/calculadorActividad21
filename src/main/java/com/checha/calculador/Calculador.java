c4c6e300798bfb56a7ddd23f0b2d2a5a530aa70a
DagmarLV <dagmar.lezama.v@uni.pe>
2023-01-05 23:21:22 -0500

    add changes for Actividad24

--- src/main/java/com/checha/calculador/Calculador.java
+++ src/main/java/com/checha/calculador/Calculador.java
@@ -1,10 +1,17 @@
 package com.checha.calculador;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
 @Service
 public class Calculador {
+    @Cacheable("sum")
     public int sum(int a, int b) {
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return a + b;
     }
 }
--- src/main/java/com/checha/calculador/CalculadorApplication.java
+++ src/main/java/com/checha/calculador/CalculadorApplication.java
@@ -1,14 +1,21 @@
 package com.checha.calculador;
-
+import com.hazelcast.client.config.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class CalculadorApplication {
-	//Este es un comentario para probar que la construcción es automática
 	public static void main(String[] args) {
 		SpringApplication.run(CalculadorApplication.class, args);
 	}
-
+	@Bean
+	public ClientConfig hazelcastClientConfig() {
+		ClientConfig clientConfig = new ClientConfig();
+		clientConfig.getNetworkConfig().addAddress("hazelcast");
+		return clientConfig;
+	}
 }
 
