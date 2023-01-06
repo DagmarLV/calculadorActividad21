6c802b88f7cfce95627dc8e5a1bfa3de5ae35e0a
DagmarLV <dagmar.lezama.v@uni.pe>
2023-01-06 06:32:07 -0500

    upload pc5-pt ii files

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+set -x
+
+NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
+NODE_PORT=$(kubectl get svc calculador-service -o=jsonpath='{.spec.ports[0].nodePort}')
+./gradlew acceptanceTest -Dcalculador.url=http://${NODE_IP}:${NODE_PORT}
